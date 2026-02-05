a9559f3b7b5ee2eaf275ba19f3171a5ac2abcb48
Rudraksh Kumar <rudrakshkumar2908@gmail.com>
2026-02-06 02:39:56 +0530

    Removed styling of anchor tag links Modified getCategory function inside createDashboard to display our dummy data, else send a 404 response Created getCategoryBySlug function which gives the category based on the slug/url paramater after categories and getItemsById function which returns items based on category_id inside db/queries.js Modified category.ejs file to display our dummy data with basic html and no styling currently

--- controllers/createDashboard.js
+++ controllers/createDashboard.js
@@ -1,12 +1,24 @@
+const db = require("../db/queries");
+
 function createDashboard(req, res) {
     res.render("dashboard", {
         title: "Dashboard",
     })
 }
 
-function categoryGet(req, res){
+async function categoryGet(req, res){
+    const {slug} = req.params;
+
+    const category = await db.getCategoryBySlug(slug);
+
+    if(category.length === 0) {
+        return res.status(404).send("OOPS! Couldn't find the category!");
+    }
+    const items = await db.getItemsById(category[0].id);
+    console.log(items)
     res.render("category", {
-        title: req.params.slug,
+        title: slug,
+        items: items
     })
 }
 
--- db/queries.js
+++ db/queries.js
@@ -1,2 +1,16 @@
 const pool = require("./pool");
 
+async function getCategoryBySlug(slug) {
+    const {rows} = await pool.query("SELECT * FROM categories WHERE name = $1", [slug]);
+    return rows;
+}
+
+async function getItemsById(id) {
+    const {rows} = await pool.query("SELECT * FROM items WHERE category_id = $1", [id]);
+    return rows;
+}
+
+module.exports = {
+    getCategoryBySlug,
+    getItemsById
+}
